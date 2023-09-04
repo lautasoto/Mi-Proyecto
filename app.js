c0c9296722207ecf193a49c834fde4911fc505e5
lautaro <magrey2015@gmail.com>
2023-09-03 22:48:13 -0300

    render

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/" , (req, res) => res.sendFile(path.join(__dirname, "views" , "home.html")))
-
-app.listen(3000, () => console.log("servidor corriendo"))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('servidor corriendo ${port}'));
